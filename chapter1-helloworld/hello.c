1323ba78259f83566c18c57ad124d4c7b1e0068b
ococa <wangchao.personal@gmail.com>
2022-08-25 18:56:48 +0800

    feat: chapter 2.1

--- chapter1-helloworld/hello.c
+++ chapter1-helloworld/hello.c
@@ -5,6 +5,7 @@ int main()
     // 输出字符串
     printf("hello world!\n");
 
+    printf("你好 ！\n");
     // 输出数字
     printf("12 + 4 = %d", 12 + 4);
     return 0;
---
+++ chapter2-var/int-float.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d \n", 10/3 * 3);
+    printf("%f \n", 10.0/3 * 3);
+
+    return 0;    
+}
