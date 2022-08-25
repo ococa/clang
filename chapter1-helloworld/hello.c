cdac33b3bdda2171c909506d3bd3ffc7a85c0b42
ococa <wangchao.personal@gmail.com>
2022-08-25 17:05:26 +0800

    init

---
+++ chapter1-helloworld/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // 输出字符串
+    printf("hello world!\n");
+
+    // 输出数字
+    printf("12 + 4 = %d", 12 + 4);
+    return 0;
+}
+
+
+// 程序框架
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 
+//     return 0;
+// }
